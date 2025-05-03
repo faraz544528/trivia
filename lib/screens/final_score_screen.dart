8ca50aa39cc63461de739741a480471d2c8567f5
faraz544528 <159261890+faraz544528@users.noreply.github.com>
2025-05-03 12:02:25 +0530

    final screen updated

--- lib/screens/final_score_screen.dart
+++ lib/screens/final_score_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travia/data/colors.dart';
+import 'package:travia/screens/home_screen.dart';
+import 'package:travia/screens/qns_screen.dart';
 import 'package:travia/widgets/custom_button.dart';
 
 class FinalScoreScreen extends StatefulWidget {
@@ -11,33 +14,80 @@ class FinalScoreScreen extends StatefulWidget {
 class _FinalScoreScreenState extends State<FinalScoreScreen> {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.sizeOf(context).height;
+    var width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Row(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [bgClr, bgClrDark],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+        child: Stack(children: [
+          Center(
+            child: Image(
+                image: AssetImage(
+                    "assets/360_F_255814073_eVeqoLhJQ31uBZ3fcgaaRcYgEtYDkYkF-removebg-preview.png")),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18, bottom: 10),
+              child: SizedBox(
+                height: height * 0.55,
+                width: width * 0.55,
+                child: Image(
+                    image: AssetImage(
+                        "assets/Screenshot_2025-05-03_002221-removebg-preview.png")),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.close),
+                Text(
+                  "Final Score",
+                  style: TextStyle(),
+                ),
+                Text(
+                  "Correct : $correct",
+                  style: TextStyle(color: Colors.green.shade900),
+                ),
+                Text(
+                  "   Incorrect : $incorrect",
+                  style: TextStyle(color: Colors.red.shade900),
+                ),
               ],
             ),
-            Text(
-              "Here will be the final score",
-              style: TextStyle(color: Colors.white),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 400),
+                  child: CustomButton(
+                    onPressed: () {},
+                    gradient: true,
+                    wdth: false,
+                    text: "Play Again",
+                  ),
+                ),
+                SizedBox(height: height * 0.01),
+                CustomButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                  gradient: true,
+                  text: "Go To Home Page",
+                ),
+              ],
             ),
-            CustomButton(
-              onPressed: () {},
-              color: true,
-              gradient: false,
-              text: "True",
-            ),
-            CustomButton(
-              onPressed: () {},
-              text: "False",
-              color: false,
-              gradient: false,
-            )
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
--- lib/screens/qns_screen.dart
+++ lib/screens/qns_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travia/data/api.dart';
 import 'package:travia/data/colors.dart';
+import 'package:travia/screens/final_score_screen.dart';
 import 'package:travia/widgets/custom_button.dart';
 
 class QnsScreen extends StatefulWidget {
@@ -12,8 +15,13 @@ class QnsScreen extends StatefulWidget {
   State<QnsScreen> createState() => _QnsScreenState();
 }
 
+var correct = 0;
+var incorrect = 0;
+
 class _QnsScreenState extends State<QnsScreen> {
   // List<Map<String, dynamic>> dataReceived = [];
+  var i = 0;
+  List<Widget> icon = [];
 
   @override
   void initState() {
@@ -23,9 +31,6 @@ class _QnsScreenState extends State<QnsScreen> {
       fetchCorrectData();
     });
   }
-
-  var i = 0;
-  var icon;
 
   fetchCorrectData() async {
     if (widget.categoryType == 'General Knowledge') {
@@ -61,56 +66,104 @@ class _QnsScreenState extends State<QnsScreen> {
 
   resultforTrue() {
     if (result[i]["correct_answer"] == "True") {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text("Correct Answer")));
+      // ScaffoldMessenger.of(context)
+      //     .showSnackBar(SnackBar(content: Text("Correct Answer")));
 
-      icon = Icon(
-        FontAwesomeIcons.check,
-        color: Colors.green,
-      );
       setState(() {
         if (i < result.length - 1) {
+          correct++;
+          icon.add(Icon(
+            FontAwesomeIcons.check,
+            color: Colors.green,
+          ));
+
           i++;
         } else {
-          i = 0;
+          Timer(
+            Duration(seconds: 3),
+            () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => FinalScoreScreen()));
+            },
+          );
         }
       });
       return icon;
     } else {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text("Incorrect Answer")));
-      icon = Icon(
-        Icons.close,
-        color: Colors.red,
-      );
+      // ScaffoldMessenger.of(context)
+      //     .showSnackBar(SnackBar(content: Text("Incorrect Answer")));
+
       setState(() {
         if (i < result.length - 1) {
+          incorrect++;
+          icon.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
           i++;
         } else {
-          i = 0;
+          Timer(
+            Duration(seconds: 3),
+            () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => FinalScoreScreen()));
+            },
+          );
         }
       });
       return icon;
     }
   }
 
-  // resultforFalse() {
-  //   if (result[i]["correct_answer"] == "False") {
+  resultforFalse() {
+    if (result[i]["correct_answer"] == "False") {
+      setState(() {
+        if (i < result.length - 1) {
+          correct++;
+          icon.add(Icon(
+            FontAwesomeIcons.check,
+            color: Colors.green,
+          ));
 
-  //   }
-  //   setState(() {
-  //     if (i < result.length - 1) {
-  //       i++;
-  //     } else {
-  //       i = 0;
-  //     }
-  //   });
-  // }
+          i++;
+        } else {
+          Timer(
+            Duration(seconds: 3),
+            () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => FinalScoreScreen()));
+            },
+          );
+        }
+      });
+      return icon;
+    } else {
+      setState(() {
+        if (i < result.length - 1) {
+          incorrect++;
+          icon.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+          i++;
+        } else {
+          Timer(
+            Duration(seconds: 3),
+            () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => FinalScoreScreen()));
+            },
+          );
+        }
+      });
+      return icon;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    debugPrint(
-        '-----------------------------------------------------> This :::: ${result != null ? result[0]["question"] : 'Raakib'}\n\n\n');
+    // debugPrint(
+    //     '-----------------------------------------------------> This :::: ${result != null ? result[0]["question"] : 'Raakib'}\n\n\n');
     debugPrint("----------------------> API RESPONSE : $result");
     return Scaffold(
       body: Center(
@@ -124,10 +177,26 @@ class _QnsScreenState extends State<QnsScreen> {
                   ? Text(result[i]['question'].toString())
                   : Text("Loading Questions....."),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [icon],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children:
+                    // Text(icon.toString()),
+                    icon.map((element) => element).toList(),
+              ),
             ),
+
+            // SizedBox(
+            //   height: 60,
+            //   width: double.infinity,
+            //   child: ListView.builder(
+            //     scrollDirection: Axis.horizontal,
+            //     itemCount: result.length,
+            //     itemBuilder: (context, index) {
+            //       return Text(icon.toString());
+            //     },
+            //   ),
+            // ),
             CustomButton(
               onPressed: () async {
                 resultforTrue();
@@ -138,13 +207,7 @@ class _QnsScreenState extends State<QnsScreen> {
             ),
             CustomButton(
               onPressed: () {
-                setState(() {
-                  if (i < result.length - 1) {
-                    i++;
-                  } else {
-                    i = 0;
-                  }
-                });
+                resultforFalse();
               },
               text: "False",
               color: false,
